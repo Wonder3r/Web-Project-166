9136064675d1e51cbdd70e4e367a08dcc4d76874
Yareth.R <99448770+Wonder3r@users.noreply.github.com>
2023-01-26 21:41:58 -0700

    Update
    Created JS function and fixed positioning of divs.

---
+++ js/index.js
@@ -0,0 +1,5 @@
+
+    function myPhotosFunction(image_thumbnail){
+        var fullImageElement = document.getElementById("img-lightbox");
+        fullImageElement.src = image_thumbnail.src;
+    }
